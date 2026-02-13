6e9c9bb8c427d29ded4f14c475c6a6b2c1ebcc23
Vladimir de Turckheim <vlad2t@hotmail.com>
2026-02-13 12:54:09 -0800

    Initial Commit

---
+++ macos-yc-countdown/macos_yc_countdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  macos_yc_countdownApp.swift
+//  macos-yc-countdown
+//
+//  Created by Vladimir de Turckheim on 13/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct macos_yc_countdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
